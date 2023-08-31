e0a5fdf1b331f70241dd171cc41b3fd020438f27
yppelu <yppelu@gmail.com>
2023-08-31 20:51:42 +0700

    Refactor the JS committed
    Decided to make it as short as possible. Not sure if it is worth it,
    but I saved about 20 rows and it works the same.

--- script.js
+++ script.js
@@ -1,61 +1,33 @@
 const form = document.querySelector('form');
-
-const emailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-const phnumberRegExp = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+const allInputs = document.querySelectorAll('input');
 
 let isInputValid;
 
-const allInputs = document.querySelectorAll('input');
+const emailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const phnumberRegExp =
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+
 allInputs.forEach(input => {
   input.addEventListener('input', () => {
-    input.classList.add('invalid-input');
     isInputValid = false;
+    input.classList.add('invalid-input');
 
-    if (input.id === 'user_fname' || input.id === 'user_lname') {
+    if (input.id === 'user_email' && !emailRegExp.test(input.value)) {
+      input.nextSibling.nextSibling.textContent = '* invalid email address';
+    } else if (input.id === 'user_phnumber' && input.value && !phnumberRegExp.test(input.value)) {
+      input.nextSibling.nextSibling.textContent = '* invalid phone number';
+    } else if (input.id === 'user_pwd' && input.value.length < 8) {
+      input.nextSibling.nextSibling.textContent = '* password must be 8 or more characters long';
+    } else if (input.id === 'user_pwd_confirm' && input.value !== document.getElementById('user_pwd').value) {
+      input.nextSibling.nextSibling.textContent = '* passwords do not match';
+    } else {
       isInputValid = true;
-    }
-    if (input.id === 'user_email') {
-      if (!emailRegExp.test(input.value)) {
-        input.nextSibling.nextSibling.textContent = "* invalid email address";
-      } else {
-        isInputValid = true;
-      }
-    }
-    if (input.id === 'user_phnumber') {
-      if (!phnumberRegExp.test(input.value)) {
-        input.nextSibling.nextSibling.textContent = "* invalid phone number";
-      } else {
-        isInputValid = true;
-      }
-      if (input.value === '' || input.value === null) {
-        input.nextSibling.nextSibling.textContent = "";
-        isInputValid = true;
-      }
-    }
-    if (input.id === 'user_pwd') {
-      if (input.value.length < 8) {
-        input.nextSibling.nextSibling.textContent = "* password must be 8 or more characters long";
-      } else {
-        isInputValid = true;
-      }
-    }
-    if (input.id === 'user_pwd_confirm') {
-      const userPassword = document.getElementById('user_pwd').value;
-      if (input.value !== userPassword) {
-        input.nextSibling.nextSibling.textContent = "* passwords do not match";
-      } else {
-        isInputValid = true;
-      }
-    }
-
-    if (isInputValid === true) {
-      input.nextSibling.nextSibling.textContent = "";
+      input.nextSibling.nextSibling.textContent = '';
       input.classList.remove('invalid-input');
-      isInputValid = true;
     }
 
     if (input.hasAttribute('required') && (input.value === '' || input.value === null)) {
-      input.nextSibling.nextSibling.textContent = "* this field is required";
+      input.nextSibling.nextSibling.textContent = '* this field is required';
       input.classList.add('invalid-input');
       isInputValid = false;
     }
@@ -65,13 +37,10 @@ allInputs.forEach(input => {
 form.addEventListener('submit', (e) => {
   allInputs.forEach(input => {
     if (input.hasAttribute('required') && (input.value === '' || input.value === null)) {
-      input.nextSibling.nextSibling.textContent = "* this field is required";
+      input.nextSibling.nextSibling.textContent = '* this field is required';
       input.classList.add('invalid-input');
       isInputValid = false;
     }
   });
-
-  if (isInputValid === false) {
-    e.preventDefault();
-  }
+  if (!isInputValid) e.preventDefault();
 });
