cc789a7452c2d981413a6c6baf35cd0ddba961bf
yppelu <yppelu@gmail.com>
2023-09-01 10:30:55 +0700

    Refactor all the code
    It's easier to comprehend and work with when each block is distinct.
    So I added a class to warning-block represents the input it is related
    to and in JS created variables for every input that needs validation
    and corresponding warning block. Then added eventListener for each of
    the inputs and created its own validating functions.
    Thus I split the logic and in the future if new inputs or validations
    will appear it won't become a problem to add function and connect it.

--- script.js
+++ script.js
@@ -1,46 +1,113 @@
-const form = document.querySelector('form');
-const allInputs = document.querySelectorAll('input');
+let isRequiredValid = true;
 
-let isInputValid;
+/* User's name validation */
+const fnameInput = document.getElementById('user_fname');
+const fnameWarningBlock = document.querySelector('.fname-warning');
+const validateFnameInput = () => validateRequiredInputs(fnameInput);
+fnameInput.addEventListener('input', () => {
+  validateFnameInput()
+    ? setValidInput(fnameInput, fnameWarningBlock)
+    : setWarning(fnameInput, fnameWarningBlock, '* this field is required');
+});
 
+/* User's email validation */
+const emailInput = document.getElementById('user_email');
+const emailWarningBlock = document.querySelector('.email-warning');
 const emailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const validateEmailInput = () => {
+  if (!validateRequiredInputs(emailInput)) return 1;
+  if (!emailRegExp.test(emailInput.value)) return 2;
+}
+emailInput.addEventListener('input', () => {
+  if (validateEmailInput() === 1) {
+    setWarning(emailInput, emailWarningBlock, '* this field is required');
+  } else if (validateEmailInput() === 2) {
+    setWarning(emailInput, emailWarningBlock, '* invalid email address');
+  } else {
+    setValidInput(emailInput, emailWarningBlock);
+  }
+});
+
+/* User's phone number validation */
+const phnumberInput = document.getElementById('user_phnumber');
+const phnumberWarningBlock = document.querySelector('.phnumber-warning');
 const phnumberRegExp =
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
-
-allInputs.forEach(input => {
-  input.addEventListener('input', () => {
-    isInputValid = false;
-    input.classList.add('invalid-input');
-
-    if (input.id === 'user_email' && !emailRegExp.test(input.value)) {
-      input.nextSibling.nextSibling.textContent = '* invalid email address';
-    } else if (input.id === 'user_phnumber' && input.value && !phnumberRegExp.test(input.value)) {
-      input.nextSibling.nextSibling.textContent = '* invalid phone number';
-    } else if (input.id === 'user_pwd' && input.value.length < 8) {
-      input.nextSibling.nextSibling.textContent = '* password must be 8 or more characters long';
-    } else if (input.id === 'user_pwd_confirm' && input.value !== document.getElementById('user_pwd').value) {
-      input.nextSibling.nextSibling.textContent = '* passwords do not match';
-    } else {
-      isInputValid = true;
-      input.nextSibling.nextSibling.textContent = '';
-      input.classList.remove('invalid-input');
-    }
-
-    if (input.hasAttribute('required') && (input.value === '' || input.value === null)) {
-      input.nextSibling.nextSibling.textContent = '* this field is required';
-      input.classList.add('invalid-input');
-      isInputValid = false;
-    }
-  });
+const validatePhnumberInput = () => {
+  return (phnumberRegExp.test(phnumberInput.value) || !phnumberInput.value);
+}
+phnumberInput.addEventListener('input', () => {
+  validatePhnumberInput()
+    ? setValidInput(phnumberInput, phnumberWarningBlock)
+    : setWarning(phnumberInput, phnumberWarningBlock, '* invalid phone number');
 });
 
+/* User's password  validation */
+const userPwdInput = document.getElementById('user_pwd');
+const userPwdWarningBlock = document.querySelector('.pwd-warning');
+const validatePwdInput = () => {
+  if (!validateRequiredInputs(userPwdInput)) return 1;
+  if (userPwdInput.value.length < 8) return 2;
+}
+userPwdInput.addEventListener('input', () => {
+  if (validatePwdInput() === 1) {
+    setWarning(userPwdInput, userPwdWarningBlock, '* this field is required');
+  } else if (validatePwdInput() === 2) {
+    setWarning(userPwdInput, userPwdWarningBlock, '* password must be 8 or more characters long');
+  } else {
+    setValidInput(userPwdInput, userPwdWarningBlock);
+  }
+});
+
+/* User's password confirm validation */
+const userPwdConfirmInput = document.getElementById('user_pwd_confirm');
+const userPwdConfirmWarningBlock = document.querySelector('.pwd-confirm-warning');
+const validatePwdConfirmInput = () => {
+  if (!validateRequiredInputs(userPwdConfirmInput)) return 1;
+  if (userPwdConfirmInput.value !== userPwdInput.value) return 2;
+}
+userPwdConfirmInput.addEventListener('input', () => {
+  if (validatePwdConfirmInput() === 1) {
+    setWarning(userPwdConfirmInput, userPwdConfirmWarningBlock, '* this field is required');
+  } else if (validatePwdConfirmInput() === 2) {
+    setWarning(userPwdConfirmInput, userPwdConfirmWarningBlock, '* passwords do not match');
+  } else {
+    setValidInput(userPwdConfirmInput, userPwdConfirmWarningBlock);
+  }
+});
+
+/* Form validation */
+const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
-  allInputs.forEach(input => {
-    if (input.hasAttribute('required') && (input.value === '' || input.value === null)) {
-      input.nextSibling.nextSibling.textContent = '* this field is required';
-      input.classList.add('invalid-input');
-      isInputValid = false;
-    }
-  });
-  if (!isInputValid) e.preventDefault();
+  if (!validateRequiredInputs(fnameInput)) {
+    setWarning(fnameInput, fnameWarningBlock, '* this field is required');
+  }
+  if (!validateRequiredInputs(emailInput)) {
+    setWarning(emailInput, emailWarningBlock, '* this field is required');
+  }
+  if (!validateRequiredInputs(userPwdInput)) {
+    setWarning(userPwdInput, userPwdWarningBlock, '* this field is required');
+  }
+  if (!validateRequiredInputs(userPwdConfirmInput)) {
+    setWarning(userPwdConfirmInput, userPwdConfirmWarningBlock, '* this field is required');
+  }
+
+  if (!isRequiredValid) e.preventDefault();
 });
+
+/* Common functions */
+function setValidInput(input, warningBlock) {
+  input.classList.remove('invalid-input');
+  warningBlock.textContent = '';
+  isRequiredValid = true;
+}
+
+function setWarning(input, warningBlock, warningText) {
+  input.classList.add('invalid-input');
+  warningBlock.textContent = warningText;
+  isRequiredValid = false;
+}
+
+function validateRequiredInputs(input) {
+  return input.value;
+}
